d7ba01519904648895659e8af633a3bc95606b89
FORMULA1031 <ryoma20031031@yahoo.co.jp>
2020-11-04 22:12:24 +0900

    lazer実装

---
+++ laser/Assets/scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            Debug.Log(hit.collider.gameObject.transform.position);
+        }
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 1);
+    }
+}
